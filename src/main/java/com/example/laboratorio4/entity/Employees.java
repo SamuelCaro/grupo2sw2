3d81d388a7b5dabf37e45eda628a5a6900c7cb65
Sebastian-RG <fullmetalliferous@gmail.com>
2020-05-11 20:32:35 -0500

    primer commit Sebastian

--- src/main/java/com/example/laboratorio4/entity/Employees.java
+++ src/main/java/com/example/laboratorio4/entity/Employees.java
@@ -1,11 +1,153 @@
 package com.example.laboratorio4.entity;
 
 import javax.persistence.*;
+import java.util.Date;
 
 
 @Entity
 @Table(name="employees")
 public class Employees {
 
-    //COMPLETAR
+    @Id
+    @Column(name="employee_id")
+    private int id;
+
+
+    @Column(name="first_name")
+    private String firstname;
+    @Column(name="last_name")
+    private String lastname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column(name="phone_number")
+    private String phonenumber;
+    @Column(name="hire_date")
+    private Date hiredate;
+    @Column(name="job_id")
+    private String jobid;
+    @Column
+    private int salary;
+    @Column(name="commission_pct")
+    private float commissionpct;
+    @OneToOne
+    @JoinColumn(name="manager_id")
+    private Employees manager;
+    @ManyToOne
+    @JoinColumn(name="department_id")
+    private Departments department;
+    @Column
+    private boolean enabled;
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public String getJobid() {
+        return jobid;
+    }
+
+    public void setJobid(String jobid) {
+        this.jobid = jobid;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public float getCommissionpct() {
+        return commissionpct;
+    }
+
+    public void setCommissionpct(float commissionpct) {
+        this.commissionpct = commissionpct;
+    }
+
+    public Employees getManager() {
+        return manager;
+    }
+
+    public void setManager(Employees manager) {
+        this.manager = manager;
+    }
+
+    public Departments getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Departments department) {
+        this.department = department;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+
 }
--- src/main/java/com/example/laboratorio4/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio4/repository/EmployeesRepository.java
@@ -1,15 +1,29 @@
 package com.example.laboratorio4.repository;
 
+import com.example.laboratorio4.dto.EmpleadoPorSalarioDto;
 import com.example.laboratorio4.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
 
+    @Query(value="SELECT e.first_name, e.last_name, jh.start_date, jh.end_date, j.job_title, e.salary\n" +
+            "            FROM hr.employees e \n" +
+            "            INNER JOIN jobs j  on e.job_id = j.job_id \n" +
+            "            INNER JOIN job_history jh on e.employee_id = jh.employee_id\n" +
+            "             where e.salary > 8000 and e.salary= ?1", nativeQuery = true)
+    List<EmpleadoPorSalarioDto> listaEmpleadosPorSalario2(int salario);
 
+    @Query(value="SELECT e.first_name, e.last_name, jh.start_date, jh.end_date, j.job_title, e.salary\n" +
+            "            FROM hr.employees e \n" +
+            "            INNER JOIN jobs j  on e.job_id = j.job_id \n" +
+            "            INNER JOIN job_history jh on e.employee_id = jh.employee_id\n" +
+            "             where e.salary > 8000", nativeQuery = true)
+    List<EmpleadoPorSalarioDto> listaEmpleadosPorSalario();
 
 
 }
