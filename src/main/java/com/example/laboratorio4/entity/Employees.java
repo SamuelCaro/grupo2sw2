35ae97bd992045e0ef750d041e7df453be3dae18
ivanmo20 <63992740+ivanmo20@users.noreply.github.com>
2020-05-11 20:36:46 -0500

    employee

--- src/main/java/com/example/laboratorio4/entity/Employees.java
+++ src/main/java/com/example/laboratorio4/entity/Employees.java
@@ -16,41 +16,7 @@ import java.util.Date;
 @Table(name="employees")
 public class Employees {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "employee_id")
-    private int employeeid;
-    @Column(name = "first_name")
-    @NotBlank
-    private String firstname;
-    @NotBlank
-    @Column(name = "Last_name",  nullable = false)
-    private String lastname;
-    @Column( nullable = false)
-    private String email;
-    @NotBlank
-    @Size(min = 8, message = "debe tener un minimo de 8 caracteres")
-    private String password;
-    @Column(name = "phone_numbe")
-    private String phonenumber;
-    @Column(name = "hire_date",  nullable = false)
-    private Date hiredate;
-
-    @Column(name = "job_id", nullable = false )
-    private Jobs jobid;
-
-    @Digits(integer = 8, fraction = 2)
-    @Positive
-    private BigDecimal salary;
-    @Column(name = "commission_pct")
-    private BigDecimal commissionpct;
-
-    @Column(name = "manager_id")
-    private Jobs managerid;
-
-    @Column(name = "department_ id")
-    private Departments departmentid;
-    private int enabled;
+   
 
 
 
